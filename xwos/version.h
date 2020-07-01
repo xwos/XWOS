cd7dc69a9a83b743e53b848cd4179e18e2389c10
隐星魂(Roy.Sun) <roy.sun@starsoul.tech>
2020-07-02 00:55:59 +0800

    patch: :bookmark: 玄武OS-V1.5.1

--- xwos/version.h
+++ xwos/version.h
@@ -41,7 +41,7 @@
  */
 #define XWOS_VERSION_MAJOR              1
 #define XWOS_VERSION_MINOR              5
-#define XWOS_VERSION_REVISION           0
+#define XWOS_VERSION_REVISION           1
 /**
  * @}
  */
