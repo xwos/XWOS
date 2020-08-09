2f564a06e244fe86bf9db8acb4bfe6b65e5490f3
隐星魂(Roy.Sun) <roy.sun@starsoul.tech>
2020-08-09 21:12:32 +0800

    test(xwam): :white_check_mark: 增加临界区的示例
      + CPU中断开关
      + 自旋锁
      + 顺序锁

--- xwam/example/lock/seqlock/xwmo.h
+++ xwam/example/lock/seqlock/xwmo.h
@@ -1,6 +1,6 @@
 /**
  * @file
- * @brief 应用模块配置
+ * @brief 示例：顺序锁
  * @author
  * + 隐星魂 (Roy.Sun) <https://xwos.tech>
  * @copyright
@@ -18,20 +18,17 @@
  * > limitations under the License.
  */
 
-#ifndef __cfg_xwam_h__
-#define __cfg_xwam_h__
+#ifndef __xwam_example_lock_seqlock_xwmo_h__
+#define __xwam_example_lock_seqlock_xwmo_h__
 
-#define XWAMCFG_example_thread_create           1
-#define XWAMCFG_example_thread_sleep            1
-#define XWAMCFG_example_timer                   1
-#define XWAMCFG_example_lock_cpuirq             1
-#define XWAMCFG_example_lock_mutex              1
-#define XWAMCFG_example_lock_spinlock           1
-#define XWAMCFG_example_lock_seqlock            1
-#define XWAMCFG_example_sync_semaphore          1
-#define XWAMCFG_example_sync_condition          1
-#define XWAMCFG_example_sync_barrier            1
-#define XWAMCFG_example_sync_flag               1
-#define XWAMCFG_example_sync_selector           1
+/******** ******** ******** ******** ******** ******** ******** ********
+ ******** ******** ********      include      ******** ******** ********
+ ******** ******** ******** ******** ******** ******** ******** ********/
+#include <xwos/standard.h>
 
-#endif /* cfg/xwam.h */
+/******** ******** ******** ******** ******** ******** ******** ********
+ ******** ********         function prototypes         ******** ********
+ ******** ******** ******** ******** ******** ******** ******** ********/
+xwer_t example_seqlock_start(void);
+
+#endif /* xwam/example/lock/seqlock/xwmo.h */
