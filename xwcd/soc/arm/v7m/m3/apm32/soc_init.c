9f60f3160a96c8afad750c0b475aada98fa79282
隐星魂 (Roy Sun) <xwos@xwos.tech>
2023-07-24 00:56:37 +0800

    feat(xwcd): :sparkles: 新增SOC APM32

---
+++ xwcd/soc/arm/v7m/m3/apm32/soc_init.c
@@ -0,0 +1,103 @@
+/**
+ * @file
+ * @brief SOC描述层：初始化
+ * @author
+ * + 隐星魂 (Roy Sun) <xwos@xwos.tech>
+ * @copyright
+ * + Copyright © 2015 xwos.tech, All Rights Reserved.
+ * > Licensed under the Apache License, Version 2.0 (the "License");
+ * > you may not use this file except in compliance with the License.
+ * > You may obtain a copy of the License at
+ * >
+ * >         http://www.apache.org/licenses/LICENSE-2.0
+ * >
+ * > Unless required by applicable law or agreed to in writing, software
+ * > distributed under the License is distributed on an "AS IS" BASIS,
+ * > WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * > See the License for the specific language governing permissions and
+ * > limitations under the License.
+ */
+
+#include <xwos/standard.h>
+#include <soc_init.h>
+
+extern xwu8_t xwos_data_lma_base[];
+extern xwu8_t xwos_data_vma_base[];
+extern xwu8_t xwos_data_vma_end[];
+
+extern xwu8_t data_lma_base[];
+extern xwu8_t data_vma_base[];
+extern xwu8_t data_vma_end[];
+
+extern xwu8_t bss_vma_base[];
+extern xwu8_t bss_vma_end[];
+
+/**
+ * @brief 重定向数据区到内存
+ */
+__xwbsp_init_code
+void soc_relocate_data(void)
+{
+        xwsz_t cnt, i;
+        xwu8_t * src;
+        xwu8_t * dst;
+
+        src = xwos_data_lma_base;
+        dst = xwos_data_vma_base;
+        if (dst != src) {
+                cnt = (xwsz_t)xwos_data_vma_end - (xwsz_t)xwos_data_vma_base;
+                for (i = 0; i < cnt; i++) {
+                        dst[i] = src[i];
+                }
+        }
+
+        src = data_lma_base;
+        dst = data_vma_base;
+        if (dst != src) {
+                cnt = (xwsz_t)data_vma_end - (xwsz_t)data_vma_base;
+                for (i = 0; i < cnt; i++) {
+                        *dst = *src;
+                        dst++;
+                        src++;
+                }
+        }
+
+        dst = bss_vma_base;
+        cnt = (xwsz_t)bss_vma_end - (xwsz_t)bss_vma_base;
+        for (i = 0; i < cnt; i++) {
+                *dst = 0;
+                dst++;
+        }
+}
+
+extern xwu8_t armv7m_ivt_lma_base[];
+extern xwu8_t armv7m_ivt_vma_base[];
+extern xwu8_t armv7m_ivt_vma_end[];
+
+/**
+ * @brief 重定向中断向量表到内存
+ */
+__xwos_init_code
+void soc_relocate_ivt(void)
+{
+        xwsz_t cnt, i;
+        xwu8_t * src;
+        xwu8_t * dst;
+
+        src = armv7m_ivt_lma_base;
+        dst = armv7m_ivt_vma_base;
+        if (dst != src) {
+                cnt = (xwsz_t)armv7m_ivt_vma_end - (xwsz_t)armv7m_ivt_vma_base;
+                for (i = 0; i < cnt; i++) {
+                        dst[i] = src[i];
+                }
+        }
+}
+
+/**
+ * @brief SOC的初始化
+ */
+__xwbsp_init_code
+void soc_init(void)
+{
+}
---
+++ xwcd/soc/arm/v7m/m3/apm32/xwosimpl_skd.h
@@ -0,0 +1,39 @@
+/**
+ * @file
+ * @brief XWOS移植实现层：调度
+ * @author
+ * + 隐星魂 (Roy Sun) <xwos@xwos.tech>
+ * @copyright
+ * + Copyright © 2015 xwos.tech, All Rights Reserved.
+ * > Licensed under the Apache License, Version 2.0 (the "License");
+ * > you may not use this file except in compliance with the License.
+ * > You may obtain a copy of the License at
+ * >
+ * >         http://www.apache.org/licenses/LICENSE-2.0
+ * >
+ * > Unless required by applicable law or agreed to in writing, software
+ * > distributed under the License is distributed on an "AS IS" BASIS,
+ * > WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * > See the License for the specific language governing permissions and
+ * > limitations under the License.
+ */
+
+#ifndef __xwosimpl_skd_h__
+#define __xwosimpl_skd_h__
+
+#ifndef __xwos_ospl_skd_h__
+#  error "This file should be included from <xwos/ospl/skd.h>."
+#endif
+
+#include <armv7m_nvic.h>
+
+static __xwbsp_inline
+void xwospl_skd_req_swcx(struct xwospl_skd * xwskd)
+{
+        XWOS_UNUSED(xwskd);
+        cm_nvic_pend_pendsv();
+        armv7m_dsb();
+        armv7m_isb();
+}
+
+#endif /* xwosimpl_skd.h */
