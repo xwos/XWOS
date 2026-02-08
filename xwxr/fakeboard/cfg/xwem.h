90a7a4f150f39b2cb5efdf22d0255abb73b0d45a
隐星曜 (Roy Sun) <xwos@xwos.tech>
2026-02-08 14:53:04 +0800

    feat(xwxr): :arrow_up: 升级配置

--- xwxr/fakeboard/cfg/xwem.h
+++ xwxr/fakeboard/cfg/xwem.h
@@ -26,11 +26,7 @@
 #define XWEMCFG_fs_littlefs                             1
 #define XWEMCFG_fs_spiffs                               0
 #define XWEMCFG_fs_dhara                                0
-
+#define XWEMCFG_cli_cherryrl                            1
 #define XWEMCFG_vm_lua                                  1
-#define XWEMCFG_vm_lua_INT_TYPE                         LUA_INT_LONG
-#define XWEMCFG_vm_lua_FLOAT_TYPE                       LUA_FLOAT_DOUBLE
-#define XWEMCFG_vm_lua_THD_STACK_SIZE                   16384
-#define XWEMCFG_vm_lua_BRDLIBS                          1
 
 #endif /* cfg/xwem.h */
